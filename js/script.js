753c827085ce08cc685d85e462f8cc28567e4e57
Efran Ahmed <79399555+efranahmed@users.noreply.github.com>
2021-07-28 14:22:37 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,31 @@
+ 
+
+
+function textChange(){
+	document.getElementsByClassName('t')[0].innerHTML="hello friends";
+}
+
+function alertMessage(){
+	alert("hiii");
+}
+
+function imageChange1(){
+
+
+	document.getElementById('img').src="images/1.jpg";
+}
+
+function imageChange2(){
+
+	document.getElementById('img').src="images/2.jpg";
+
+}
+function alertMessage2(){
+	alert("Wellcome to my page");
+}
+function writeMassage(){
+	document.write("Wellcome to our page");
+}
+function consoleMessage(){
+	console.log("Wellcome to our page");
+}
